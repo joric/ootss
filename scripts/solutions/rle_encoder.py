1d91b4a6d5321fbe47615648d79e0bde4820e011
joric <joric@users.noreply.github.com>
2026-06-22 22:42:54 +0500

    updated files

--- scripts/solutions/rle_encoder.py
+++ scripts/solutions/rle_encoder.py
@@ -1,16 +1,13 @@
-from re import sub
-import shutil
+import re, shutil
 
-if __name__ == "__main__":
-    rle = lambda s: sub(r'(.)\1*', lambda m: m[1] + (str(len(m[0])) if len(m[0]) > 1 else ''), s)
-    shutil.copy2('solutions.csv', 'solutions.csv.bak')
+rle = lambda s: re.sub(r'(.)\1*', lambda m: m[1] + str(len(m[0]) if len(m[0]) > 1 else ''), s)
 
-    with open('solutions.csv','r') as infile:
-        lines = infile.read().splitlines()
-    outfile = open('solutions.csv','w')
-    for line in lines:
-        if line:
-            name, sequence = line.split(',', 1)
-            outfile.write(f"{name},{rle(sequence)}\n")
+shutil.copy2('solutions.csv', 'solutions.csv.bak')
 
-    print("Done.")
+with open('solutions.csv') as f:
+    rows = [l.split(',', 1) for l in f.read().splitlines() if l]
+
+with open('solutions.csv', 'w') as f:
+    f.writelines(f"{n},{rle(s)}\n" for n, s in rows)
+
+print("Done.")
